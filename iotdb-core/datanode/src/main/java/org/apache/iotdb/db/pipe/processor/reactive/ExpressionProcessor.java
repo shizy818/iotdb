c6acbe729fba1877490acc9e68c33c18b929ddd2
shizy <shizy04@gmail.com>
2025-04-15 17:01:38 +0800

    graalvm

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/processor/reactive/ExpressionProcessor.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/processor/reactive/ExpressionProcessor.java
@@ -19,17 +19,35 @@
 
 package org.apache.iotdb.db.pipe.processor.reactive;
 
+import org.apache.iotdb.common.rpc.thrift.TSStatus;
+import org.apache.iotdb.commons.conf.IoTDBConstant;
 import org.apache.iotdb.commons.consensus.DataRegionId;
+import org.apache.iotdb.commons.exception.IllegalPathException;
+import org.apache.iotdb.commons.exception.IoTDBException;
+import org.apache.iotdb.commons.path.PartialPath;
+import org.apache.iotdb.db.auth.AuthorityChecker;
+import org.apache.iotdb.db.conf.IoTDBDescriptor;
+import org.apache.iotdb.db.conf.rest.IoTDBRestServiceDescriptor;
+import org.apache.iotdb.db.pipe.connector.protocol.writeback.WriteBackConnector;
 import org.apache.iotdb.db.pipe.event.common.row.PipeResetTabletRow;
 import org.apache.iotdb.db.pipe.event.common.tablet.PipeInsertNodeTabletInsertionEvent;
 import org.apache.iotdb.db.pipe.event.common.tablet.PipeRawTabletInsertionEvent;
+import org.apache.iotdb.db.protocol.rest.table.v1.handler.QueryDataSetHandler;
 import org.apache.iotdb.db.protocol.session.IClientSession;
+import org.apache.iotdb.db.protocol.session.InternalClientSession;
 import org.apache.iotdb.db.protocol.session.SessionManager;
 import org.apache.iotdb.db.queryengine.plan.Coordinator;
+import org.apache.iotdb.db.queryengine.plan.analyze.ClusterPartitionFetcher;
+import org.apache.iotdb.db.queryengine.plan.analyze.schema.ClusterSchemaFetcher;
+import org.apache.iotdb.db.queryengine.plan.execution.ExecutionResult;
+import org.apache.iotdb.db.queryengine.plan.execution.IQueryExecution;
 import org.apache.iotdb.db.queryengine.plan.planner.LocalExecutionPlanner;
 import org.apache.iotdb.db.queryengine.plan.relational.metadata.Metadata;
 import org.apache.iotdb.db.queryengine.plan.relational.sql.parser.SqlParser;
+import org.apache.iotdb.db.queryengine.plan.statement.Statement;
+import org.apache.iotdb.db.queryengine.plan.statement.metadata.ShowTimeSeriesStatement;
 import org.apache.iotdb.db.storageengine.StorageEngine;
+import org.apache.iotdb.db.utils.SetThreadName;
 import org.apache.iotdb.pipe.api.PipeProcessor;
 import org.apache.iotdb.pipe.api.access.Row;
 import org.apache.iotdb.pipe.api.annotation.TableModel;
@@ -44,8 +62,10 @@ import org.apache.iotdb.pipe.api.event.Event;
 import org.apache.iotdb.pipe.api.event.dml.insertion.TabletInsertionEvent;
 import org.apache.iotdb.pipe.api.event.dml.insertion.TsFileInsertionEvent;
 
+import org.apache.iotdb.rpc.TSStatusCode;
 import org.apache.tsfile.common.constant.TsFileConstant;
 import org.apache.tsfile.enums.TSDataType;
+import org.apache.tsfile.read.common.block.TsBlock;
 import org.apache.tsfile.utils.BitMap;
 import org.apache.tsfile.write.schema.MeasurementSchema;
 import org.graalvm.polyglot.Context;
@@ -53,7 +73,10 @@ import org.graalvm.polyglot.Value;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.ws.rs.core.Response;
 import java.io.IOException;
+import java.time.ZoneId;
+import java.util.Optional;
 
 import static org.apache.iotdb.commons.pipe.config.constant.PipeProcessorConstant.PROCESSOR_EXPR;
 
@@ -63,11 +86,11 @@ public class ExpressionProcessor implements PipeProcessor {
   private static final Logger LOGGER = LoggerFactory.getLogger(ExpressionProcessor.class);
 
   private static final Metadata metadata = LocalExecutionPlanner.getInstance().metadata;
-  private static final SqlParser RELATIONAL_SQL_PARSER = new SqlParser();
+  private static final SqlParser sqlParser = new SqlParser();
 
   private static final Coordinator COORDINATOR = Coordinator.getInstance();
   private static final SessionManager SESSION_MANAGER = SessionManager.getInstance();
-  private IClientSession clientSession;
+  private IClientSession treeSession;
 
   private static final Context graalVMConext = Context.create("js");
   private String expr;
@@ -96,19 +119,21 @@ public class ExpressionProcessor implements PipeProcessor {
         StorageEngine.getInstance()
             .getDataRegion(new DataRegionId(environment.getRegionId()))
             .getDatabaseName();
-
     outputTimeSeries = dataBaseName + TsFileConstant.PATH_SEPARATOR + "t2";
-    columnNameStringList = new String[] {"c0", "c1"};
-    measurementSchemaList =
-        new MeasurementSchema[] {
-          new MeasurementSchema("c0", TSDataType.FLOAT),
-          new MeasurementSchema("c1", TSDataType.INT32)
-        };
-    valueColumnTypes = new TSDataType[] {TSDataType.FLOAT, TSDataType.INT32};
 
-    //    measurementSchemaList =
-    //            new MeasurementSchema[columnNameStringList.length];
-    //    valueColumnTypes = new TSDataType[columnNameStringList.length];
+    treeSession =
+            new InternalClientSession(
+                    String.format(
+                            "%s_%s_%s_%s_tree",
+                            WriteBackConnector.class.getSimpleName(),
+                            environment.getPipeName(),
+                            environment.getCreationTime(),
+                            environment.getRegionId()));
+    treeSession.setUsername(AuthorityChecker.SUPER_USER);
+    treeSession.setClientVersion(IoTDBConstant.ClientVersion.V_1_0);
+    treeSession.setZoneId(ZoneId.systemDefault());
+
+    executeShowSeriesForTreeModel(dataBaseName);
 
     valueColumns = new Object[columnNameStringList.length];
     bitMaps = new BitMap[columnNameStringList.length];
@@ -149,8 +174,8 @@ public class ExpressionProcessor implements PipeProcessor {
 
   @Override
   public void close() throws Exception {
-    if (clientSession != null) {
-      SESSION_MANAGER.closeSession(clientSession, COORDINATOR::cleanupQueryExecution);
+    if (treeSession != null) {
+      SESSION_MANAGER.closeSession(treeSession, COORDINATOR::cleanupQueryExecution);
     }
   }
 
@@ -195,6 +220,79 @@ public class ExpressionProcessor implements PipeProcessor {
               columnNameStringList));
     } catch (IOException e) {
       throw new RuntimeException(e);
+    }
+  }
+
+  private void executeShowSeriesForTreeModel(final String databaseName) {
+    String fullDeviceName = databaseName + TsFileConstant.PATH_SEPARATOR + "t1" + ".*";
+    Long queryId = null;
+    try {
+      Statement showTimeSeriesStatement =
+              new ShowTimeSeriesStatement(new PartialPath(fullDeviceName), false);
+
+      treeSession.setDatabaseName(databaseName);
+      treeSession.setSqlDialect(IClientSession.SqlDialect.TREE);
+      SESSION_MANAGER.registerSession(treeSession);
+
+      queryId = SESSION_MANAGER.requestQueryId();
+      ExecutionResult result = Coordinator.getInstance()
+              .executeForTreeModel(
+                      showTimeSeriesStatement,
+                      queryId,
+                      SESSION_MANAGER.getSessionInfo(treeSession),
+                      "",
+                      ClusterPartitionFetcher.getInstance(),
+                      ClusterSchemaFetcher.getInstance(),
+                      IoTDBDescriptor.getInstance().getConfig().getQueryTimeoutThreshold(),
+                      false);
+      if (result.status.code != TSStatusCode.SUCCESS_STATUS.getStatusCode()
+              && result.status.code != TSStatusCode.DATABASE_ALREADY_EXISTS.getStatusCode()
+              && result.status.code != TSStatusCode.DATABASE_CONFLICT.getStatusCode()) {
+        LOGGER.error(
+                "Show TimeSeries error, statement: {}, result status : {}.",
+                showTimeSeriesStatement, result.status);
+      }
+
+      IQueryExecution queryExecution = COORDINATOR.getQueryExecution(queryId);
+      Optional<TsBlock> optionalTsBlock = queryExecution.getBatchResult();
+      if (optionalTsBlock.isPresent()) {
+        TsBlock tsBlock = optionalTsBlock.get();
+        int rowCount = tsBlock.getPositionCount();
+        columnNameStringList = new String[rowCount];
+        measurementSchemaList = new MeasurementSchema[rowCount];
+        valueColumnTypes = new TSDataType[rowCount];
+
+        for(int i = 0; i < rowCount; i++) {
+          // Timeseries
+          String[] fullName = tsBlock.getColumn(0).getBinary(i).toString().split("[.]");
+          columnNameStringList[i] = fullName[fullName.length - 1];
+                  ;
+          // Column Type
+          String columnType = tsBlock.getColumn(3).getBinary(i).toString();
+          valueColumnTypes[i] = getTsDataType(columnType);
+          // Measurement
+          measurementSchemaList[i] = new MeasurementSchema(
+                 columnNameStringList[i], valueColumnTypes[i]
+          );
+        }
+      }
+    } catch (IoTDBException e) {
+      throw new RuntimeException(e);
+    } finally {
+      if (queryId != null) {
+        COORDINATOR.cleanupQueryExecution(queryId);
+      }
+    }
+  }
+
+  private TSDataType getTsDataType(String tsType) {
+    switch (tsType) {
+      case "INT32":
+        return TSDataType.INT32;
+      case "FLOAT":
+        return TSDataType.FLOAT;
+      default:
+        return null;
     }
   }
 }
