ad77294ec81f3d72732bc6fb184fd47c77bbe873
shizy <shizy04@gmail.com>
2025-11-18 10:52:50 +0800

    add ReplicaHint

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/plan/relational/planner/distribute/TableDistributedPlanGenerator.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/plan/relational/planner/distribute/TableDistributedPlanGenerator.java
@@ -98,6 +98,7 @@ import org.apache.iotdb.db.queryengine.plan.relational.sql.ast.Expression;
 import org.apache.iotdb.db.queryengine.plan.relational.sql.ast.FunctionCall;
 import org.apache.iotdb.db.queryengine.plan.relational.sql.ast.Insert;
 import org.apache.iotdb.db.queryengine.plan.relational.sql.ast.SymbolReference;
+import org.apache.iotdb.db.queryengine.plan.relational.utils.hint.Hint;
 import org.apache.iotdb.db.queryengine.plan.statement.component.Ordering;
 import org.apache.iotdb.db.schemaengine.table.DataNodeTableCache;
 import org.apache.iotdb.db.schemaengine.table.DataNodeTreeViewSchemaUtils;
@@ -1882,6 +1883,7 @@ public class TableDistributedPlanGenerator
 
   public static class PlanContext {
     final Map<PlanNodeId, NodeDistribution> nodeDistributionMap;
+    final Map<String, Hint> hintMap;
     boolean hasExchangeNode = false;
     boolean hasSortProperty = false;
     boolean pushDownGrouping = false;
@@ -1891,6 +1893,12 @@ public class TableDistributedPlanGenerator
 
     public PlanContext() {
       this.nodeDistributionMap = new HashMap<>();
+      this.hintMap = new HashMap<>();
+    }
+
+    public PlanContext(Map<String, Hint> hintMap) {
+      this.nodeDistributionMap = new HashMap<>();
+      this.hintMap = hintMap;
     }
 
     public NodeDistribution getNodeDistribution(PlanNodeId nodeId) {
