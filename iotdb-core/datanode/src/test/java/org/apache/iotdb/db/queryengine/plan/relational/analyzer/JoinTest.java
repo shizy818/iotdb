c7c76a7ca1b35784d378ed413fa49acc8b923f16
shizy <shizy04@gmail.com>
2026-04-21 14:43:33 +0800

    code format issue

--- iotdb-core/datanode/src/test/java/org/apache/iotdb/db/queryengine/plan/relational/analyzer/JoinTest.java
+++ iotdb-core/datanode/src/test/java/org/apache/iotdb/db/queryengine/plan/relational/analyzer/JoinTest.java
@@ -188,7 +188,6 @@ public class JoinTest {
     List<JoinNode.EquiJoinClause> joinCriteria =
         Collections.singletonList(
             new JoinNode.EquiJoinClause(Symbol.of("time"), Symbol.of("time_0")));
-
     assertJoinNodeEquals(
         joinNode,
         INNER,
